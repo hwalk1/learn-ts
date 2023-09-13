ba79c4b89cb4fe6eb5673048dc9f47cddc5a6ce3
hwalk1 <haydenjwalker1@gmail.com>
2023-09-13 12:14:04 +1000

    function variable types

---
+++ 02basics/myfunction.ts
@@ -0,0 +1,13 @@
+function addTwo(num: number) {
+  return num + 2;
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase;
+}
+
+function signUp(name: string, email: string, password: string) {}
+
+addTwo(4);
+getUpper("test");
+signUp("test", "test", "test");
